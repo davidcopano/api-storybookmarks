e5adff0a461eeb93071274ecbc3df5aada27f62c
David Copano <davidcopano96@gmail.com>
2020-05-03 12:31:43 +0200

    Haciendo registro

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::post('login', 'Auth\LoginController@validateLogin')->name('login');
+Route::post('register', 'Auth\RegisterController@create')->name('register');
 
 Route::middleware('auth:api')->get('/', function (Request $request) {
     return $request->user();
