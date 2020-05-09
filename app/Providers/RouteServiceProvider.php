81906dea94984048a194e35999a5eb4659dea490
David Copano <davidcopano@dcopano.xyz>
2020-05-09 17:45:40 +0200

    Cambios en servidor

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -58,9 +58,13 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
+	Route::middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+
+        /* Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/web.php')); */
     }
 
     /**
