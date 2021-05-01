c0207be2f5fb59f967ada8f5171fc277846eb869
David Copano <davidcopano96@gmail.com>
2021-05-01 17:16:37 +0200

    Habilitado CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -19,7 +20,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-	\Fruitcake\Cors\HandleCors::class,
+	    \Fruitcake\Cors\HandleCors::class,
+        CorsMiddleware::class
     ];
 
     /**
@@ -62,7 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'json.response' => \App\Http\Middleware\ForceJson::class
+        'json.response' => \App\Http\Middleware\ForceJson::class,
+        'cors' => CorsMiddleware::class
     ];
 
     /**
